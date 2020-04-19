fc635d0a78251269870fe26632fac9d860a4fbd6
Grant Duffy <grantd@buchanan-a.com>
2020-04-19 13:40:01 -0700

    Added kwargs to function calling in camera

--- camera.py
+++ camera.py
@@ -5,8 +5,9 @@ from sh import gphoto2 as gp
 class Camera:
 
     def __getattr__(self, item):
-        arg = item.replace('_', '-')
-        return lambda: gp(f'--{arg}')
+        command = item.replace('_', '-')
+        args_gen = lambda kwargs: ' '.join([f'--{k} {kwargs[k]}' for k in kwargs])
+        return lambda **kwargs: gp(f'--{command} {args_gen(kwargs)}')
 
 
 camera = Camera()
