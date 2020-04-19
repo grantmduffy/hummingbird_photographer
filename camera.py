fd0cdd6f42b7513ee3b4a21e66fd9dce25e78f5c
Grant Duffy <grantd@buchanan-a.com>
2020-04-19 15:00:21 -0700

    Simplified code in camera

--- camera.py
+++ camera.py
@@ -5,18 +5,9 @@ from sh import gphoto2 as gp
 class Camera:
 
     def __getattr__(self, item):
-        command = item.replace('_', '-')
-
-        def ret_func(**kwargs):
-            params = [f'--{command}'] + [x for y in zip([f'--{k}' for k in kwargs],
-                                                        [f'{kwargs[k]}' for k in kwargs]) for x in y]
-            print(params)
-            return gp(params)
-
-        return ret_func
-        
-        # return lambda **kwargs: gp([f'--{command}'] + [x for y in zip([f'--{k}' for k in kwargs],
-        #                                                               [f'{kwargs[k]}' for k in kwargs]) for x in y])
+        return lambda **kwargs: gp([f'--{item.replace("_", "-")}'] +
+                                   [x for y in zip([f'--{k}' for k in kwargs],
+                                                   [f'{kwargs[k]}' for k in kwargs]) for x in y])
 
 
 camera = Camera()
