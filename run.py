0da80aec5eaa3bbde02cb7e5288223bb1740b208
Grant Duffy <grantd@buchanan-a.com>
2020-04-24 22:46:42 -0700

    Run file updated

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 # Main run file - should take images and run inference
+from time import sleep
+from shutil import copy
+from camera import capture
+from mnv3_predict import predict_hum
 
 print('RUN')
+
+while True:
+    img_path = capture('~/shared/')
+    if predict_hum(img_path):
+        copy(img_path, '~/shared/hum/')
+    else:
+        copy(img_path, '~/shared/not_hum/')
+    sleep(10)
