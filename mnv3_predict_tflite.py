a22d1dea8d9bc7e2fc110a83f4182b280d8628b4
tommduffy <tommduffy@gmail.com>
2020-04-25 16:05:27 -0700

    added tflite convert and inference

--- mnv3_predict_tflite.py
+++ mnv3_predict_tflite.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
+# import tensorflow as tf
+import tflite_runtime.interpreter as tflite
 import numpy as np
 
 from PIL import Image
@@ -8,13 +9,14 @@ from PIL import Image
 IMAGE_SHAPE = (224, 224)
 
 # get the labels
-labels_path = tf.keras.utils.get_file('ImageNetLabels.txt',
-                                      'https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
-imagenet_labels = list(np.array(open(labels_path).read().splitlines()))
-hummingbird_index = imagenet_labels.index('hummingbird')
+# labels_path = tf.keras.utils.get_file('ImageNetLabels.txt',
+#                                       'https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
+# imagenet_labels = list(np.array(open(labels_path).read().splitlines()))
+# hummingbird_index = imagenet_labels.index('hummingbird')
+hummingbird_index = 95
 
 # Load TFLite model and allocate tensors.
-interpreter = tf.lite.Interpreter(model_path="humm.tflite")
+interpreter = tflite.Interpreter(model_path="humm.tflite")
 interpreter.allocate_tensors()
 
 # Get input and output tensors.
@@ -22,15 +24,15 @@ input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 # Test the TensorFlow Lite model on random input data.
-input_shape = input_details[0]['shape']
-input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-interpreter.set_tensor(input_details[0]['index'], input_data)
-
-interpreter.invoke()
-
+# input_shape = input_details[0]['shape']
+# input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+# interpreter.set_tensor(input_details[0]['index'], input_data)
+#
+# interpreter.invoke()
+#
 # The function `get_tensor()` returns a copy of the tensor data.
 # Use `tensor()` in order to get a pointer to the tensor.
-tflite_results = interpreter.get_tensor(output_details[0]['index'])
+# tflite_results = interpreter.get_tensor(output_details[0]['index'])
 
 
 def predict_hum(filename):
