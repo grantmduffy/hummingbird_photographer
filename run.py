e21b1d515314c60da99c5a0dd349b7d58e90e102
Grant Duffy <grantd@buchanan-a.com>
2020-04-25 20:31:35 -0700

    Verbose run.py

--- run.py
+++ run.py
@@ -8,11 +8,19 @@ print('RUN')
 
 while True:
     img_path = capture_preview('/home/pi/shared/')
+    print(f'Capture Taken: {img_path}...', end='')
     if predict_hum(img_path):
+        print('Hummingbird Found!!!')
         # copy(img_path, '/home/pi/shared/hum/')
         for file in capture():
+            print(f'\tFile: {file}: ', end='')
             if predict_hum(file):
+                print('Is Hummingbird')
                 copy(file, '/home/pi/shared/hum/')
             else:
+                print('Not Hummingbird')
                 copy(file, '/home/pi/shared/not_hum/')
+    else:
+        print('No Hummingbird')
+
     sleep(10)
