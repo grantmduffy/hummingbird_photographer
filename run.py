955af9e78999f2c29e59095f2af0a3eafecfd8b7
Grant Duffy <grantd@buchanan-a.com>
2020-04-25 20:22:26 -0700

    fixed paths. Runs run.py on restart

--- run.py
+++ run.py
@@ -7,12 +7,12 @@ from mnv3_predict_tflite import predict_hum
 print('RUN')
 
 while True:
-    img_path = capture_preview('home/pi/shared/')
+    img_path = capture_preview('/home/pi/shared/')
     if predict_hum(img_path):
-        # copy(img_path, 'home/pi/shared/hum/')
+        # copy(img_path, '/home/pi/shared/hum/')
         for file in capture():
             if predict_hum(file):
-                copy(file, 'home/pi/shared/hum/')
+                copy(file, '/home/pi/shared/hum/')
             else:
-                copy(file, 'home/pi/shared/not_hum/')
+                copy(file, '/home/pi/shared/not_hum/')
     sleep(10)
