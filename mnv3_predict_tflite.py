76ef42e60ce881e42326c6f88c857904d62aacac
tommduffy <tommduffy@gmail.com>
2020-05-03 21:09:24 -0700

    updated tflite model and fixed predict code for image swap

--- mnv3_predict_tflite.py
+++ mnv3_predict_tflite.py
@@ -1,22 +1,20 @@
-# import tensorflow as tf
-import tflite_runtime.interpreter as tflite
+import sys
 import numpy as np
-
 from PIL import Image
 
+win32 = sys.platform == 'win32'
+if win32:
+    import tensorflow as tf
+    from pathlib import Path
+else:
+    import tflite_runtime.interpreter as tflite
 
-# get the model
-IMAGE_SHAPE = (224, 224)
-
-# get the labels
-# labels_path = tf.keras.utils.get_file('ImageNetLabels.txt',
-#                                       'https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
-# imagenet_labels = list(np.array(open(labels_path).read().splitlines()))
-# hummingbird_index = imagenet_labels.index('hummingbird')
-hummingbird_index = 95
 
 # Load TFLite model and allocate tensors.
-interpreter = tflite.Interpreter(model_path="humm.tflite")
+if win32:
+    interpreter = tf.lite.Interpreter(model_path='humm.tflite')
+else:
+    interpreter = tflite.Interpreter(model_path="humm.tflite")
 interpreter.allocate_tensors()
 
 # Get input and output tensors.
@@ -24,7 +22,7 @@ input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 # Test the TensorFlow Lite model on random input data.
-# input_shape = input_details[0]['shape']
+input_shape = input_details[0]['shape']
 # input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
 # interpreter.set_tensor(input_details[0]['index'], input_data)
 #
@@ -36,7 +34,7 @@ output_details = interpreter.get_output_details()
 
 
 def predict_hum(filename):
-    img = Image.open(filename).resize((224, 224))
+    img = Image.open(filename).resize(input_shape[2:0:-1])
     img_data = np.array(img, dtype=np.float32) / 255.0
 
     interpreter.set_tensor(input_details[0]['index'], img_data[np.newaxis, ...])
@@ -45,13 +43,15 @@ def predict_hum(filename):
     # The function `get_tensor()` returns a copy of the tensor data.
     # Use `tensor()` in order to get a pointer to the tensor.
     tflite_results = interpreter.get_tensor(output_details[0]['index'])
-    predicted_class = np.argmax(tflite_results[0], axis=-1)
-    return predicted_class == hummingbird_index
+    predicted_class = np.round(tflite_results[0])
+    return predicted_class
 
-#
-# filename = 'images/humm1.jpg'
-# # filename = 'images/birdfeeder.jpg'
-# if predict_hum(filename):
-#     print("Yes, it's a hummingbird")
-# else:
-#     print("No go - no hummingbird")
+
+if __name__ == '__main__':
+
+    for label in ['background', 'hummingbird']:
+        files = Path(f'training_data/{label}')
+        print(files)
+        files = files.glob('*.JPG')
+        for img_file in files:
+            print(f'{str(img_file)}, label: {label}, prediction: {predict_hum(str(img_file))}')
